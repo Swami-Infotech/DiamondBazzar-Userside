dd5527af259afbbd49fb3e6f22a12369f8141d1a
vyas dipali <dipali@swamiinfotech.in>
2024-07-20 11:42:12 +0530

    DemandPostPreview2 Add

---
+++ Diamond-Userside/src/app/DemandPost/DemandPostPreview2/view/demand-post-preview2/demand-post-preview2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demand-post-preview2',
+  templateUrl: './demand-post-preview2.component.html',
+  styleUrls: ['./demand-post-preview2.component.css']
+})
+export class DemandPostPreview2Component {
+
+}
--- Diamond-Userside/src/app/app.module.ts
+++ Diamond-Userside/src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Home/View/home/home.component';
-import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { LoginComponent } from './Login/View/login/login.component';
 import { OtpComponent } from './Otp/View/otp/otp.component';
 import { SignupComponent } from './Signup/view/signup/signup.component';
@@ -19,9 +16,6 @@ import { NavComponent } from './Nav/view/nav/nav.component';
 import { FooterComponent } from './footer/view/footer/footer.component';
 import { ProductComponent } from './Product details/view/product/product.component';
 import { ProductsComponent } from './products/view/products/products.component';
-import { ToastrModule } from 'ngx-toastr';
-import { InterceptorTokenService } from './Common/interceptor-token.service';
-import { FormsModule } from '@angular/forms';
 import { wishlistComponent } from './Wishlist/View/wishlist/wishlist.component';
 import { CalculatorComponent } from './Calculator/view/calculator/calculator.component';
 import { DemandComponent } from './Demand/View/demand/demand.component';
@@ -32,10 +26,16 @@ import { Polishpost2Component } from './Polish/Polishpost2/view/polishpost2/poli
 import { RoughPostComponent } from './Rough/Roughpost/view/rough-post/rough-post.component';
 import { RoughPost1Component } from './Rough/RoughPost1/view/rough-post1/rough-post1.component';
 
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { ToastrModule } from 'ngx-toastr';
+import { InterceptorTokenService } from './Common/interceptor-token.service';
+import { DemandPostPreview2Component } from './DemandPost/DemandPostPreview2/view/demand-post-preview2/demand-post-preview2.component';
 
-
-export function HttpLoaderFactory(HttpClient:HttpClient){
-  return new TranslateHttpLoader(HttpClient)
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 @NgModule({
@@ -61,7 +61,8 @@ export function HttpLoaderFactory(HttpClient:HttpClient){
     Polishpost1Component,
     Polishpost2Component,
     RoughPostComponent,
-    RoughPost1Component
+    RoughPost1Component,
+    DemandPostPreview2Component
   ],
   imports: [
     BrowserModule,
@@ -70,24 +71,26 @@ export function HttpLoaderFactory(HttpClient:HttpClient){
     AppRoutingModule,
     HttpClientModule,
     ToastrModule.forRoot({
-      timeOut:5000,
-      positionClass:'toast-top-right',
-      preventDuplicates:true
+      timeOut: 5000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true
     }),
-    TranslateModule.forRoot(
-      {
-      loader:{
-        provide:TranslateLoader,
-        useFactory:HttpLoaderFactory,
-        deps:[HttpClient]
-
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
       }
-    }
-    )
+    })
   ],
-  providers: [HttpClient,{
-    provide:HTTP_INTERCEPTORS,useClass:InterceptorTokenService,multi:true
-  }],
+  providers: [
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorTokenService,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
